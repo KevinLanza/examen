833aa1299ab45e171e2125b0e309bf986a106734
KevinLanza <klanza03@gmail.com>
2014-05-12 09:08:46 -0600

    Update ejercicio2.java

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -3,13 +3,13 @@ public class ejercicio2 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner (System.in);
-		System.out.println("Ingrese un numero");
-		int num1 = s.nextInt();
-		System.out.println("Ingrese una cadena de texto");
+		System.out.println("Ingrese un numero");//Ingresamos un numero
+		int s = s.nextInt();
+		System.out.println("Ingrese una cadena de texto");//Ingresamos una cadena de texto
 		String texto = s.next();
-		int t = 0;
+		int t = 0;//Declaramos un contador
 		
-		while (t < num1)
+		while (t < num1)//Con el ciclo while hacemos el ciclo necesario para repetir lo necesario
 			{
 			System.out.println(t + num1);
 			t++;
