f90c9b19bc087416f3e0433fcb3799f8dd4cd44f
KevinLanza <klanza03@gmail.com>
2014-05-19 08:13:28 -0600

    Update ejercicio2.java

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -4,15 +4,15 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner (System.in);
-		System.out.println("Ingrese un numero");
+		System.out.println("Ingrese un numero");//El Usuario agrega un numero
 		int num = s.nextInt();
-		System.out.println("Ingrese una cadena de texto");
+		System.out.println("Ingrese una cadena de texto");//El usuario agrega una cadena de texto
 		String text = s.next ();
 		
-		int cont = 0;
+		int cont = 0;//Se establece una variable a utilizar en el ciclo
 			
 		
-		while (cont < num)
+		while (cont < num)//Se establece el ciclo a evaluar
 		{
 			System.out.println(text);
 			cont++;
